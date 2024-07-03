2185e475b575619ccb352b13017046e080e8be7e
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-07-03 13:15:37 +0530

    done

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -6,27 +6,28 @@ import pandas as pd
 import joblib
 
 model = joblib.load('xgbpipe.joblib')
-st.title('Did they survive? :ship:')
+st.title('Did they make it through? :ship:')
 # PassengerId,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
-passengerid = st.text_input("Input Passenger ID", '123456') 
-pclass = st.selectbox("Choose class", [1,2,3])
-name  = st.text_input("Input Passenger Name", 'John Smith')
-sex = st.select_slider("Choose sex", ['male','female'])
-age = st.slider("Choose age",0,100)
-sibsp = st.slider("Choose siblings",0,10)
-parch = st.slider("Choose parch",0,2)
-ticket = st.text_input("Input Ticket Number", "12345") 
-fare = st.number_input("Input Fare Price", 0,1000)
-cabin = st.text_input("Input Cabin", "C52") 
-embarked = st.select_slider("Did they Embark?", ['S','C','Q'])
+passengerid = st.text_input("PASSENGER ID ENTRY", '123456') 
+pclass = st.selectbox("CLASS SELECTION", [1,2,3])
+name  = st.text_input("PASSENGER NAME ENTRY", 'Jason Brown')
+sex = st.select_slider("SEXUAL IDENTITY", ['male','female'])
+age = st.slider("YOUR AGE",0,100)
+sibsp = st.slider("SIBLINGS SELECTION",0,10)
+parch = st.slider("PARENTS/CHILDREN ONBOARD",0,2)
+ticket = st.text_input("INPUT TICKET NUMBER", "12345") 
+fare = st.number_input("FARE PRICE INPUT", 0,1000)
+cabin = st.text_input("INPUT CABIN", "C52") 
+embarked = st.select_slider("DID THEY SET SAIL?", ['S','C','Q'])
 
 def predict(): 
-    row = np.array([passengerid,pclass,name,sex,age,sibsp,parch,ticket,fare,cabin,embarked]) 
-    X = pd.DataFrame([row], columns = columns)
+    row = [passengerid, pclass, name, sex, age, sibsp, parch, ticket, fare, cabin, embarked]
+    X = pd.DataFrame([row], columns=columns)
     prediction = model.predict(X)
     if prediction[0] == 1: 
         st.success('Passenger Survived :thumbsup:')
     else: 
         st.error('Passenger did not Survive :thumbsdown:') 
 
-trigger = st.button('Predict', on_click=predict)
+trigger = st.button('PREDICT', on_click=predict)
+
