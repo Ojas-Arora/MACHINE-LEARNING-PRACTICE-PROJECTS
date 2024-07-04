e6eeb97954c0f86d7f0fd939f46aa158359d1c20
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-07-04 12:25:47 +0530

    Create test.py

---
+++ demo/test.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+def main():
+    fig = plt.figure()
+    plt.plot([1, 2, 3], [1, 4, 9])
+    plt.title("Test Plot")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
