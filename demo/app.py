53579fcd08239feea64240d46569a37cc29cf562
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-07-09 17:19:50 +0530

    done

--- demo/app.py
+++ demo/app.py
@@ -3,24 +3,27 @@ import pandas as pd
 import plotly.graph_objects as go
 from streamlit_lottie import st_lottie
 import json
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
 def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
         return json.load(f)
 
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-
+# Load Netflix data
 netflix_data = pd.read_csv('netflix_titles.csv')
 
-
-tfidf = TfidfVectorizer(stop_words='english')
+# Fill missing descriptions with empty strings
 netflix_data['description'] = netflix_data['description'].fillna('')
+
+# Create TF-IDF matrix
+tfidf = TfidfVectorizer(stop_words='english')
 tfidf_matrix = tfidf.fit_transform(netflix_data['description'])
 
-from sklearn.metrics.pairwise import linear_kernel
+# Compute cosine similarity
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
+# Create reverse mapping of indices and movie titles
 indices = pd.Series(netflix_data.index, index=netflix_data['title']).drop_duplicates()
 
 def get_recommendations(title, cosine_sim=cosine_sim):
@@ -31,54 +34,37 @@ def get_recommendations(title, cosine_sim=cosine_sim):
     movie_indices = [i[0] for i in sim_scores]
     return netflix_data.iloc[movie_indices]
 
-#print(get_recommendations('Dark'))
-#tfidf_matrix.shape
 def Table(df):
-    fig=go.Figure(go.Table( columnorder = [1,2,3],
-          columnwidth = [10,28],
-            header=dict(values=[' Title','Description'],
-                        line_color='black',font=dict(color='black',size= 19),height=40,
-                        fill_color='#dd571c',#
-                        align=['left','center']),
-                cells=dict(values=[df.title,df.description],
-                       fill_color='#ffdac4',line_color='grey',
-                           font=dict(color='black', family="Lato", size=16),
-                       align='left')))
-    fig.update_layout(height=500, title ={'text': "Top 10 Movie Recommendations", 'font': {'size': 22}},title_x=0.5
-                     )
-    return st.plotly_chart(fig,use_container_width=True)
-movie_list = netflix_data['title'].values
+    fig = go.Figure(go.Table(
+        columnorder=[1, 2, 3],
+        columnwidth=[10, 28],
+        header=dict(values=['Title', 'Description'],
+                    line_color='black', font=dict(color='black', size=19), height=40,
+                    fill_color='#dd571c', align=['left', 'center']),
+        cells=dict(values=[df.title, df.description],
+                   fill_color='#ffdac4', line_color='grey',
+                   font=dict(color='black', family="Lato", size=16), align='left')
+    ))
+    fig.update_layout(height=500, title={'text': "Top 10 Movie Recommendations", 'font': {'size': 22}}, title_x=0.5)
+    return st.plotly_chart(fig, use_container_width=True)
 
+# Streamlit app
+st.header('Netflix Movie Recommendation System')
 
-####################################################################
-#streamlit
-##################################################################
-
-st.header('Netflix Movie Recommendation System ')
+# Load and display Lottie animation
 lottie_coding = load_lottiefile("m4.json")
-st_lottie(
-    lottie_coding,
-    speed=1,
-    reverse=False,
-    loop=True,
-    quality="low",height=220
-)
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
-)
+st_lottie(lottie_coding, speed=1, reverse=False, loop=True, quality="low", height=220)
 
+# Dropdown for movie selection
+movie_list = netflix_data['title'].values
+selected_movie = st.selectbox("Type or select a movie from the dropdown", movie_list)
+
+# Display recommendations on button click
 if st.button('Show Recommendation'):
     recommended_movie_names = get_recommendations(selected_movie)
-    #list_of_recommended_movie = recommended_movie_names.to_list()
-   # st.write(recommended_movie_names[['title', 'description']])
     Table(recommended_movie_names)
-    
-st.write('  '
-         )
-st.write(' ')
 
+# Checkbox for EDA link
 EDA = st.checkbox('Show Netflix Exploratory Data Analysis')
-if EDA :
-    st.write(
-        "check out this [link](https://www.kaggle.com/code/rushikeshdane20/in-depth-analysis-of-netflix-with-plotly)")
+if EDA:
+    st.write("Check out this [link](https://www.kaggle.com/code/rushikeshdane20/in-depth-analysis-of-netflix-with-plotly)")
